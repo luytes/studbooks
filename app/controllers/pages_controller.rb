30f7423f118a74a402309f4f9d21b9845724eff1
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-08-19 20:43:23 +0800

    added basic search form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @book = Book.new
+    @user = User.new
   end
 
   def privacy_policy
