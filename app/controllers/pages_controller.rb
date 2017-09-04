29c0e11e3a6be64281bf04b35a05d976fc16eed3
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-09-04 15:17:03 +0200

    set the params conditions for the search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @users = User.new
+    # with the nested attributes
     @users.books.build
   end
 
