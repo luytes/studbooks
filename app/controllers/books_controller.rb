3eee778a29eeeec020843e48a5d13583e1071800
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-10-01 10:28:03 +0200

    send an ajax request to the site, with the new value of the select requesting the data. The site gets the data from database and sorts it. After that, my javascript redraws the cards in my index page with the sorted book response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,8 @@ class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def index
+    @title = params.dig("book", "title")
+    @university = params.dig("users", "university")
     # Using present? method
     # if params[:book][:title].present? && params[:users][:university].present?
     #   @books = Book.where({title: params[:book][:title]})
@@ -17,27 +19,71 @@ class BooksController < ApplicationController
 
     # Using the DIG method
     # The dig method won't look further if a key is nil, which avoids this kind of error.
-    if params.dig("book", "title").nil? && params.dig("users", "university").nil?
+    # if params.dig("book", "title").nil? && params.dig("users", "university").nil?
+    #   @books = Book.where({title: params.dig("book", "title")})
+    #   .joins(:user).where(users: {university: params.dig("users", "university")})
+    # elsif !params.dig("book", "title").nil? && params.dig("users", "university").nil?
+    #   @books = Book.where({title: params.dig("book", "title")})
+    # elsif params.dig("book", "title").nil? && !params.dig("users", "university").nil?
+    #   @books = Book.joins(:user).where(users: {university: params.dig("users", "university")})
+    # else
+    #   @books = Book.all
+    # end
+
+    # Using the == "" method
+    if (params.dig("book", "title") != "" ) && (params.dig("users", "university") != "" )
       @books = Book.where({title: params.dig("book", "title")})
       .joins(:user).where(users: {university: params.dig("users", "university")})
-    elsif !params.dig("book", "title").nil? && params.dig("users", "university").nil?
+    elsif (params.dig("book", "title") != "" ) && (params.dig("users", "university") == "" )
       @books = Book.where({title: params.dig("book", "title")})
-    elsif params.dig("book", "title").nil? && !params.dig("users", "university").nil?
+    elsif (params.dig("book", "title") == "" ) && (params.dig("users", "university") != "" )
       @books = Book.joins(:user).where(users: {university: params.dig("users", "university")})
     else
       @books = Book.all
     end
-    # @books.order(price_cents: sort_order.to_sym) if params[:sort].present?
+
     case params[:sort]
       when "Price Descending"
-        @books.order(price_cents: "DESC")
+        if (params[:title] != "" ) && (params[:university] != "" )
+          @books = Book.where({title: params[:title]})
+          .joins(:user).where(users: {university: params[:university]}).order(price_cents: "DESC")
+        elsif (params[:title] != "" ) && (params[:university] == "" )
+          @books = Book.where({title: params[:title]}).order(price_cents: "DESC")
+        elsif (params[:title] == "" ) && (params[:university] != "" )
+          @books = Book.joins(:user).where(users: {university: params[:university]}).order(price_cents: "DESC")
+        else
+          @books = Book.all.order(price_cents: "DESC")
+        end
       when "Price Ascending"
-        @books.order(price_cents: "ASC")
-      else
-        @books.sort_by(&:created_at)
+        if (params[:title] != "" ) && (params[:university] != "" )
+          @books = Book.where({title: params[:title]})
+          .joins(:user).where(users: {university: params[:university]}).order(price_cents: "ASC")
+        elsif (params[:title] != "" ) && (params[:university] == "" )
+          @books = Book.where({title: params[:title]}).order(price_cents: "ASC")
+        elsif (params[:title] == "" ) && (params[:university] != "" )
+          @books = Book.joins(:user).where(users: {university: params[:university]}).order(price_cents: "ASC")
+        else
+          @books = Book.all.order(price_cents: "ASC")
+        end
+      when "Best Results"
+        if (params[:title] != "" ) && (params[:university] != "" )
+          @books = Book.where({title: params[:title]})
+          .joins(:user).where(users: {university: params[:university]}).sort_by(&:created_at)
+        elsif (params[:title] != "" ) && (params[:university] == "" )
+          @books = Book.where({title: params[:title]}).sort_by(&:created_at)
+        elsif (params[:title] == "" ) && (params[:university] != "" )
+          @books = Book.joins(:user).where(users: {university: params[:university]}).sort_by(&:created_at)
+        else
+          @books = Book.all.sort_by(&:created_at)
+        end
     end
     @publish_year = @books.map { |p| p.publish_year }.uniq
     @condition = @books.map { |c| c.condition }.uniq
+    respond_to do |format|
+      format.html
+      format.json { render json: @books }
+    end
+    # render json: {data: @books}
   end
 
   def show
