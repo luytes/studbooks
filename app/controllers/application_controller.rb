6ed800fe7dacf31d7348d1b10c20a89024a53741
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-10-08 12:48:08 +0200

    made an if statement to automatically fill in the university when the user is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :age, :university,
+                                                       :country, :picture])
     # only if you want the user to be able to update his username
     # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
